1fb108f8a0482e69c590abf5431c14e194fdc320
I'm Sanket :) <153053395+BeingSaka@users.noreply.github.com>
2025-02-21 20:27:50 +0530

    Add files via upload

---
+++ DB Dimension Exploration.sql
@@ -0,0 +1,67 @@
+
+/*
+===============================================================================
+Dimensions Exploration
+===============================================================================
+Purpose:
+    - To explore the structure of dimension tables.
+	
+SQL Functions Used:
+    - DISTINCT
+    - ORDER BY
+===============================================================================
+*/
+
+-- Retrieve a list of unique countries from which customers originate
+SELECT DISTINCT 
+    country 
+FROM gold.dim_customers
+ORDER BY country;
+
+-- Retrieve a list of unique categories, subcategories, and products
+SELECT DISTINCT 
+    category, 
+    subcategory, 
+    product_name 
+FROM gold.dim_products where category is not null
+ORDER BY category, subcategory, product_name;
+
+-- Retrieve the count of distinct products sold  
+SELECT COUNT(DISTINCT product_key) AS count  
+FROM gold.fact_sales;  
+
+-- Calculate the average customer ID for each country  
+SELECT  
+    country,  
+    AVG(customer_id) AS sum  
+FROM gold.dim_customers  
+GROUP BY country;  
+
+-- Count the number of marital status records for each gender  
+SELECT  
+    gender,  
+    COUNT(marital_status) AS sum  
+FROM gold.dim_customers  
+GROUP BY gender;  
+
+-- Retrieve all customer records  
+SELECT *  
+FROM gold.dim_customers;  
+
+-- Retrieve the earliest and latest birthdate of customers  
+SELECT  
+    MIN(birthdate) AS minBirthDate,  
+    MAX(birthdate) AS maxBirthDate  
+FROM gold.dim_customers;  
+
+-- Retrieve the top 5 customer records  
+SELECT TOP 5 *  
+FROM gold.dim_customers;  
+
+-- Retrieve a list of unique customer creation dates  
+SELECT DISTINCT  
+    create_date  
+FROM gold.dim_customers;  
+
+
+select distinct create_date from gold.dim_customers;
---
+++ DB Measure Exploration.sql
@@ -0,0 +1,62 @@
+/*
+===============================================================================
+Measures Exploration (Key Metrics)
+===============================================================================
+Purpose:
+    - To calculate aggregated metrics (e.g., totals, averages) for quick insights.
+    - To identify overall trends or spot anomalies.
+
+SQL Functions Used:
+    - COUNT(), SUM(), AVG()
+===============================================================================
+*/
+
+-- Find the Total Sales
+SELECT SUM(sales_amount) AS total_sales FROM gold.fact_sales
+
+-- Find how many items are sold
+SELECT SUM(quantity) AS total_quantity FROM gold.fact_sales
+
+-- Find the average selling price
+SELECT AVG(price) AS avg_price FROM gold.fact_sales
+
+-- Find the Total number of Orders
+SELECT COUNT(order_number) AS total_orders FROM gold.fact_sales
+SELECT COUNT(DISTINCT order_number) AS total_orders FROM gold.fact_sales
+
+-- Find the total number of products
+SELECT COUNT(product_name) AS total_products FROM gold.dim_products
+
+-- Find the total number of customers
+SELECT COUNT(customer_key) AS total_customers FROM gold.dim_customers;
+
+-- Find the total number of customers that has placed an order
+SELECT COUNT(DISTINCT customer_key) AS total_customers FROM gold.fact_sales;
+
+-- Generate a Report that shows all key metrics of the business
+SELECT 'Total Sales' AS measure_name, SUM(sales_amount) AS measure_value FROM gold.fact_sales
+UNION ALL
+SELECT 'Total Quantity', SUM(quantity) FROM gold.fact_sales
+UNION ALL
+SELECT 'Average Price', AVG(price) FROM gold.fact_sales
+UNION ALL
+SELECT 'Total Orders', COUNT(DISTINCT order_number) FROM gold.fact_sales
+UNION ALL
+SELECT 'Total Products', COUNT(DISTINCT product_name) FROM gold.dim_products
+UNION ALL
+SELECT 'Total Customers', COUNT(customer_key) FROM gold.dim_customers;
+
+-- Conclusion: Summary of Key Business Metrics  
+
+-- The report provides an overview of essential business indicators,  
+-- helping stakeholders assess performance and make data-driven decisions.  
+
+-- - Total Sales: 29,356,250, representing overall revenue generated.  
+-- - Total Quantity: 60,423, indicating the total number of units sold.  
+-- - Average Price: 486, showing the average selling price per product.  
+-- - Total Orders: 27,659, representing the number of unique transactions.  
+-- - Total Products: 295, highlighting the variety of products available.  
+-- - Total Customers: 18,484, reflecting the customer base and engagement.  
+
+-- This report enables better analysis of sales trends, customer reach,  
+-- and product performance, supporting strategic business decisions.  
